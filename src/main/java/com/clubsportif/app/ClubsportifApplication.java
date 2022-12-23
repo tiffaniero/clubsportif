f868686ce5a0962de2e5d232b60f3fa71462c783
tiffaniero <tiffanie.roger01@gmail.com>
2022-12-23 14:11:25 +0100

    WIP - Add Angular and SpringBoot

---
+++ src/main/java/com/clubsportif/app/ClubsportifApplication.java
@@ -0,0 +1,13 @@
+package com.clubsportif.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubsportifApplication {
+
+	public static void main(String[] args){
+		SpringApplication.run(ClubsportifApplication.class, args);
+	}
+
+}
